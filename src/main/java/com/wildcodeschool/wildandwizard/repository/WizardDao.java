77ac67a29044f23c2cbd421c1c142c56f5b79b86
Antoine Zudas <mrgluptv@gmail.com>
2019-11-18 11:43:45 +0100

    C'est bon ?

--- src/main/java/com/wildcodeschool/wildandwizard/repository/WizardDao.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/WizardDao.java
@@ -1,18 +1,20 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 import com.wildcodeschool.wildandwizard.entity.Wizard;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.List;
 
+
 public interface WizardDao {
 
-    Wizard save(Wizard entity);
+    Wizard save(Wizard wizard);
 
     Wizard findById(Long id);
 
     List<Wizard> findAll();
 
-    Wizard update(Wizard entity);
+    Wizard update(Wizard wizard);
 
     void deleteById(Long id);
 }
